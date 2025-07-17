14320789a6f83aebfe269f5db53c5463b22d3b7c
JErBerlin <jeronir@hotmail.com>
2025-07-17 20:15:40 +0200

    use of slog instead of log; separate panics from fatal; clearer naming

--- 34_errorhandling/store/store.go
+++ 34_errorhandling/store/store.go
@@ -4,17 +4,18 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"log"
+	"log/slog"
 )
 
-type Error string
-
-func (e Error) Error() string {
-	return string(e)
+type errNotFound struct {
+	key string
 }
 
-const errNotFound = Error("couldn't find any stored value for the given key")
+func (e errNotFound) Error() string {
+	return fmt.Sprintf("couldn't find any stored value for the key: %s", e.key)
+}
 
 func main() {
 	s := NewStore()
@@ -23,21 +24,24 @@ func main() {
 	// and store a new pair, if the key is unknown
 	// but produce a panic if getting an error other that not found
 	_, err := s.Get("red")
+	var errnf errNotFound
 	if err != nil {
-		if err == errNotFound {
-			s.Set("red", "big loser")
-		} else {
-			log.Fatal(err)
+		if !errors.As(err, &errnf) {
+			slog.Error("unexpected error", "err", err)
+			panic(err)
 		}
+		slog.Error("first try", "err", errnf)
+		s.Set("red", "big loser")
 	}
 
 	// check for the stored key
 	// and produce a panic if the pair was not stored
 	v, err := s.Get("red")
 	if err != nil {
-		log.Fatal(err)
+		slog.Error("retrieving sotred key failed", "err", err)
+		panic(err)
 	}
-	fmt.Printf("%v\n", v)
+	slog.Info("value retrieved for red", "value", v)
 }
 
 type Store struct {
@@ -58,5 +62,5 @@ func (s *Store) Get(k string) (string, error) {
 	if v, ok := s.keys[k]; ok {
 		return v, nil
 	}
-	return "", errNotFound
+	return "", errNotFound{k}
 }
